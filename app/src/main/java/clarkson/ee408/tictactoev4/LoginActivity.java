e60192bf7062e73d79c3aec3e5f391def201b368
andre <anjharri@clarkson.edu>
2023-12-05 08:33:44 -0500

    Milestone 4 part 2

---
+++ app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
@@ -0,0 +1,93 @@
+package clarkson.ee408.tictactoev4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import clarkson.ee408.tictactoev4.model.User;
+import clarkson.ee408.tictactoev4.socket.Request;
+import clarkson.ee408.tictactoev4.socket.Response;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private Gson gson;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Getting UI elements
+        Button loginButton = findViewById(R.id.buttonLogin);
+        Button registerButton = findViewById(R.id.buttonRegister);
+        usernameField = findViewById(R.id.editTextUsername);
+        passwordField = findViewById(R.id.editTextPassword);
+
+        gson = new GsonBuilder().serializeNulls().create();
+
+        //Adding Handlers
+        loginButton.setOnClickListener(view -> handleLogin());
+        registerButton.setOnClickListener(view -> gotoRegister());
+    }
+
+    /**
+     * Process login input and pass it to {@link #submitLogin(User)}
+     */
+    public void handleLogin() {
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Username and password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        User user = new User("hellooo", "passwords", "name", true);
+        submitLogin(user);
+    }
+
+    /**
+     * Sends a LOGIN request to the server
+     * @param user User object to login
+     */
+    public void submitLogin(User user) {
+        // TODO: Send a LOGIN request, If SUCCESS response, call gotoPairing(), else, Toast the error message from sever
+        Request.RequestType.LOGIN();
+        if(Response.ResponseStatus.SUCCESS){
+            gotoPairing(user.getUsername());
+        }
+
+        else {
+            Toast.makeText(this, "Failed Login", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Switch the page to {@link PairingActivity}
+     * @param username the data to send
+     */
+    public void gotoPairing(String username) {
+        Intent intent = new Intent(LoginActivity.this, PairingActivity.class);
+        intent.putExtra("USERNAME", username);
+        startActivity(intent);
+
+    }
+
+    /**
+     * Switch the page to {@link RegisterActivity}
+     */
+    public void gotoRegister() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+
+    }
+}
